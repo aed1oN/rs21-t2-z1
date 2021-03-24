552ffb1707344d1b701a6d218d4b1032f3731820
Edin Ičanović <eicanovic1@etf.unsa.ba>
2021-03-24 16:00:48 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,13 @@
+package sample;
+
+import javafx.scene.control.Label;
+
+public class Controller {
+
+    public Label helloWorld;
+
+    public void onClick() {
+        helloWorld.setText("Hello World!");
+
+    }
+}
